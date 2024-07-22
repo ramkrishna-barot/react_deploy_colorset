827f3d89d75c09009ab88022297bb7494d397de3
rbarot-seniorinsight <rbarot@seniorinsight.com>
2024-07-22 16:07:32 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+    const [color, setColor] = useState('');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='colorBoxParent'>
+        <div className='colorBox' style={{backgroundColor: color}}>
+          <p className='colorName'>{color===''?'Empty value':color}</p>
+        </div><br/>
+        <input className='colorText' type="text" id="txtColor" value={color} 
+               placeholder='Enter color name' onChange={(e) => setColor(e.target.value)} />
     </div>
   );
 }
